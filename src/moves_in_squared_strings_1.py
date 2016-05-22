d8007bfbbc2ae0c0ff73990241966642afb97983
Pavel Karateev <karateev.pavel@ya.ru>
2016-05-22 22:36:13 +0300

    "Moves in squared strings" kata WIP

---
+++ src/moves_in_squared_strings_1.py
@@ -0,0 +1,17 @@
+"""Moves in squared strings (I)
+http://www.codewars.com/kata/56dbe0e313c2f63be4000b25
+
+
+"""
+
+
+def vert_mirror(strng):
+    pass
+
+
+def hor_mirror(strng):
+    pass
+
+
+def oper(fct, s):
+    pass
