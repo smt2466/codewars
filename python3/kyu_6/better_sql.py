5b51a7ff96ea24693667e4a0820455366e032129
Pavel Karateev <karateev.pavel@ya.ru>
2016-06-21 10:49:49 +0300

    Fixes travis build

--- python3/kyu_6/better_sql.py
+++ python3/kyu_6/better_sql.py
@@ -1,3 +1,5 @@
+# pylint: disable=missing-docstring
+
 """I Liked the SQL Better...
 https://www.codewars.com/kata/53d2c97d7152a59b64001033
 
@@ -17,19 +19,24 @@ Your database should have a table named MOVIES that contains the following data:
 import os
 import sqlite3
 
-conn = sqlite3.connect(os.path.join('tmp', 'movies.db'))
-cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE MOVIES
-                  (name TEXT, year INTEGER, rating INTEGER)''')
+def main():
+    conn = sqlite3.connect(os.path.join('tmp', 'movies.db'))
+    cursor = conn.cursor()
 
-cursor.execute('''INSERT INTO movies VALUES
-                  ('Rise of the Planet of the Apes', 2011, 77),
-                  ('Dawn of the Planet of the Apes', 2014, 91),
-                  ('Alien', 1979, 97),
-                  ('Aliens', 1986, 98),
-                  ('Mad Max', 1979, 95),
-                  ('Mad Max 2: The Road Warrior', 1981, 100)''')
+    cursor.execute('''CREATE TABLE MOVIES
+                      (name TEXT, year INTEGER, rating INTEGER)''')
 
-conn.commit()
-conn.close()
+    cursor.execute('''INSERT INTO movies VALUES
+                      ('Rise of the Planet of the Apes', 2011, 77),
+                      ('Dawn of the Planet of the Apes', 2014, 91),
+                      ('Alien', 1979, 97),
+                      ('Aliens', 1986, 98),
+                      ('Mad Max', 1979, 95),
+                      ('Mad Max 2: The Road Warrior', 1981, 100)''')
+
+    conn.commit()
+    conn.close()
+
+if __name__ == '__main__':
+    main()
